f4ddfa79dd7b0be258e3d3352fd4cc58d39ad04d
Vitaly Bragilevsky <vitaly.bragilevsky@jetbrains.com>
2024-11-25 14:15:33 +0200

    Use TODO comments

--- src/bin/NN.rs
+++ src/bin/NN.rs
@@ -5,12 +5,12 @@ use code_timing_macros::time_snippet;
 use const_format::concatcp;
 use adv_code_2024::*;
 
-const DAY: &str = "NN";
+const DAY: &str = "NN"; // TODO: Fill the day
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
 
 const TEST: &str = "\
 <TEST-INPUT>
-";
+"; // TODO: Add the test input
 
 fn main() -> Result<()> {
     start_day(DAY);
@@ -19,9 +19,12 @@ fn main() -> Result<()> {
     println!("=== Part 1 ===");
 
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        Ok(0)
+        // TODO: Solve Part 1 of the puzzle
+        let answer = reader.lines().flatten().count();
+        Ok(answer)
     }
 
+    // TODO: Set the expected answer for the test input
     assert_eq!(0, part1(BufReader::new(TEST.as_bytes()))?);
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
